b685b5be558961554eb51d406fa78f0a74607885
optimizm0 <businessman999777555@gmail.com>
2025-04-01 16:06:00 +0100

    --chore:: widget responsiveness fix

--- src/routes/signin/index.jsx
+++ src/routes/signin/index.jsx
@@ -2,8 +2,14 @@ import styles from "./styles.module.css";
 import { LogoFull } from "../../assets";
 import { SigninForm } from "./components";
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const Signin = () => {
+	const isloggedIn = useSelector((state) => !!state?.user?.value?.jwt);
+	if (isloggedIn) {
+		return <Navigate to="/dashboard" />;
+	}
 	return (
 		<main className={styles.main}>
 			<div className={styles.container}>
